12659d9385ce4dc27095d0113a644e5f3eb66590
NataliaT-94 <nattecheira@gmail.com>
2024-09-18 18:40:03 -0300

    feria-emprendedores js-modal

--- feria-emprendedores/gulpfile.js
+++ feria-emprendedores/gulpfile.js
@@ -5,16 +5,24 @@ export function hola(done){//colocando el export ya te da acceso para ejecutarlo
     done();// es la funcion que finaliza la ejecucion de la funcion
 }*/
 
-import {src, dest, watch} from 'gulp' //src: es la funcion que nos va a permitir acceder a los archivos fuentes,donde estan ubicados, dest: es donde se van a almacenar una vez que hayan sido procesados
+import {src, dest, watch, series} from 'gulp' //src: es la funcion que nos va a permitir acceder a los archivos fuentes,donde estan ubicados, dest: es donde se van a almacenar una vez que hayan sido procesados
+//series: te permite ejecutar una tarea y despues otra. puedes ejecutar multiples tareas con esta funcion
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
+export function js(done){
+    src('src/js/app.js')
+        .pipe(dest('build/js'))
+
+    done()
+}
+
 export function css(done){
-    src('src/scss/app.scss')
+    src('src/scss/app.scss', {sourcemaps: true})//sourcemaps: siver para generar un archivo interno para que cuando pongas inspeccionar el elemento te indique en que lina de scss esta ubicada
         .pipe( sass().on('error', sass.logError))//una vez que ubica la funcion lo ejecuta el sass. Si hay un error mostrar cual
-        .pipe( dest('build/css') )//luego lo almacena
+        .pipe( dest('build/css', {sourcemaps: true}) )//luego lo almacena
 
     done()
 }
@@ -22,4 +30,9 @@ export function css(done){
 export function dev(){
     // watch('src/scss/app.scss', css)//revia la carpeta y ejecuta la funcion css
     watch('src/scss/**/*.scss',css) //revisa todas las carpetas y archivos, si hay cambios lo ejecuta
+    watch('src/js/**/*.js',js) //revisa todas las carpetas y archivos, si hay cambios lo ejecuta
 }
+
+// export default parallel(js, css, dev) // te permite ejecutar todas las funciones al mismo tiempo
+
+export default series(js, css, dev)// package.json sector script-"dev": "gulp" // te permite ejecutar un funcion y cuando termina ejecuta la siguiente
---
+++ feria-emprendedores/src/js/app.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function(){
+    crearGaleria();
+})
+
+function crearGaleria(){
+
+    const CANTIDAD_IMAGENES = 16;
+    const galeria = document.querySelector('.galeria-imagenes');
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
+      const imagen = document.createElement('IMG')
+      imagen.src = `src/img/galeria/imagen${i}.jpg`
+      imagen.alt = 'Imagen Galeria'
+
+      // Event Handler
+
+      imagen.onclick = function(){
+        mostrarImagen(i)
+      }
+
+      galeria.appendChild(imagen)
+        
+    }
+}
+
+function mostrarImagen(i){
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/galeria/imagen${i}.jpg`
+    imagen.alt = 'Imagen Galeria'
+
+    // Generar Modal
+
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal //no hace falta colocar function porq no lleva parametro 
+
+    // Boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
+    // Agregar al HTML
+
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+}
+
+function cerrarModal(){
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    
+
+    setTimeout(()=>{
+        modal?.remove()//el ? indica que si es que existe modal remover
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500);
+}
