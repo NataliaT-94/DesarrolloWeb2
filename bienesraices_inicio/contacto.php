125a0cf96f30ce4c5b6b39535a161ccacfe69a02
NataliaT-94 <nattecheira@gmail.com>
2024-10-13 18:16:54 -0300

    bienesraices bd-php- templates y funciones

--- bienesraices_inicio/contacto.php
+++ bienesraices_inicio/contacto.php
@@ -1,36 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="build/css/app.css">
-    
-    <title>Contacto</title>
-</head>
-<body>
-    <header class="header">
-        <div class="contenedor contenido-header">
-            <div class="barra">
-                <a href="index.html">
-                    <img src="build/img/logo.svg" alt="logotipo de bienes Raices">
-                </a>
+<?php 
+    require 'includes/funciones.php';
 
-                <div class="mobile-menu">
-                    <img src="build/img/barras.avif" alt="icono menu responsive">
-                </div>
-
-                <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg">
-                    <nav class="navegacion">
-                        <a href="nosotros.html">Nosotros</a>
-                        <a href="anuncios.html">Anuncios</a>
-                        <a href="blog.html">Blog</a>
-                        <a href="contacto.html">Contacto</a>
-                    </nav>
-                </div>
-            </div><!----  .barra  ---->
-        </div>
-    </header>
+    incluirTemplate('header');
+?>
     
     <main class="contenedor seccion">
         <h1>Contacto</h1>
@@ -101,18 +73,7 @@
         </form>
     </main>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros .html">Nosotros</a>
-                <a href="anuncios.html">Anuncios</a>
-                <a href="blog.html">Blog</a>
-                <a href="contacto.html">Contacto</a>
-            </nav>
-        </div>
-        <p class="copyright">Todos los derechos Reservados 2024 &copy;</p>
-    </footer>
 
-    <script src="build/js/bundle.min.js"></script>
-</body>
-</html>
+    <?php 
+        incluirTemplate('footer' );
+    ?>
