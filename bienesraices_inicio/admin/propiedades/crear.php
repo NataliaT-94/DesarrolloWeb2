8ffb47e718c498988b6971217dadb08b1331c16e
NataliaT-94 <nattecheira@gmail.com>
2024-10-17 15:45:02 -0300

    bienesraices sql base de datos- insertar datos a la base de datos

--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -1,52 +1,167 @@
 <?php 
-    require '../../includes/funciones.php';
 
+    //Base de datos
+    require '../../includes/config/database.php';
+    $db = conectarDB();
+
+    //Consultar para obtener los vendedores
+    $consulta = "SELECT * FROM vendedores";
+    $resultado = mysqli_query($db, $consulta);
+
+    //para acceder a la info del POSt o GET
+    // echo "<pre>";
+    // var_dump($SERVER);//contiene toda la informacion del servidor
+    // var_dump($_POST);
+    // var_dump($_GET);
+    // echo "<pre>";
+
+    //Array con mensajes de errores
+    $errores = [];
+
+    $titulo = '';
+    $precio = '';
+    $descripcion = '';
+    $habitaciones = '';
+    $banio = '';
+    $estacionamiento = '';
+    $vendedorId = '';
+
+  //Ejecutar el codigo despues de que el usuario envia el formulario
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      // Opcional: Mostrar los datos recibidos para depuración
+        //   echo "<pre>";
+        //   var_dump($_POST);
+        //   echo "</pre>";
+  
+      // Sanitizar y asignar variables
+      $titulo = $_POST['titulo'];
+      $precio = $_POST['precio'];
+      $descripcion = $_POST['descripcion'];
+      $habitaciones = $_POST['habitaciones'];
+      $banio = $_POST['banio'];
+      $estacionamiento = $_POST['estacionamiento'];
+      $vendedorId = $_POST['vendedor'];
+      $creado = date('y/m/d');
+
+      if(!$titulo){//si no hay titulo
+        $errores[] = "Debes añadir un titulo";
+      }
+
+      if(!$precio){
+        $errores[] = "Debes añadir un precio";
+      }
+
+      if(strlen($descripcion) < 50){
+        $errores[] = "La descripcion es oblidgatoria y debe tener al menos 50 caracteres";
+      }
+
+      if(!$habitaciones){
+        $errores[] = "El numero de habitaciones es obligatorio";
+      }
+
+      if(!$banio){
+        $errores[] = "El numero de banios es obligatorio";
+      }
+
+      if(!$estacionamiento){
+        $errores[] = "El numero de estacionamientos es obligatorio";
+      }
+
+      if(!$vendedorId){
+        $errores[] = "Elije un vendedor";
+      }
+
+    //   echo "<pre>";
+    //   var_dump($errores);
+    //   echo "</pre>";
+
+    //   exit;
+
+      //Revisar que el array de $errores ese vacio 
+
+      if(empty($errores)){//si el array de $errores esta vacio
+
+            // insertar base de datos
+            $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, banio, estacionamiento, creado, vendedorId) 
+            VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$banio', '$estacionamiento', $creado, '$vendedorId')";
+        
+
+            //echo $query;
+
+            $resultado = mysqli_query($db, $query);
+
+            if($resultado){
+                //Redireccionar al usuario
+                header('Location: http://localhost/GitHub/DesarrolloWeb2/bienesraices_inicio/admin/');
+
+            } else{
+                echo "Error: " . mysqli_error($db);
+            }
+        }
+  }
+
+
+
+    require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
     
     
     <main class="contenedor seccion">
-        <h1>Crear</h1>
+        <h1>Crear</h1> 
 
         <a href="http://localhost/GitHub/DesarrolloWeb2/bienesraices_inicio/admin/" class="boton boton-verde">Volver</a>
 
-        <form class="formulario">
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+
+        <?php endforeach; ?>
+
+        <form class="formulario" method="POST" action="http://localhost/GitHub/DesarrolloWeb2/bienesraices_inicio/admin/propiedades/crear.php">
+            <!-- GET: guarda la informacion en la URL, se comunica con el name: titulo// no es recomendable para mandar datos a un servidor, porque expone los datos en la URL -->
+            <!-- POST: maneja internamente en el archivo, se comunica con el name: titulo // envia datos de forma segura -->
             <fieldset>
                 <legend>Informacion General</legend>
                 
                 <label for="titulo">Titulo:</label>
-                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value="<?php echo $titulo; ?>">
                 
                 <label for="precio">Precio:</label>
-                <input type="number" id="precio" placeholder="Precio Propiedad">
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $preci0; ?>">
                 
                 <label for="imagen">Imagen:</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
                 
                 <label for="descripcion">Descripcion:</label>
-                <textarea id="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Informacion Propiedad</legend>
                 
                 <label for="habitaciones">Habitaciones:</label>
-                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
                 
                 <label for="baño">Baños:</label>
-                <input type="number" id="baño" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="baño" name="banio" placeholder="Ej: 3" min="1" max="9" value="<?php echo $banio; ?>">
                 
                 <label for="estacionamiento">Estacionamiento:</label>
-                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">
 
             </fieldset>
 
             <fieldset>
                 <legend>Vendedor</legend>
                 
-                <select>
-                    <option value="1">Natt</option>
-                    <option value="2">Braian</option>
+                <select name="vendedor" value="<?php echo $vendedorId; ?>">
+                    <option value="">-- Seleccione --</option>
+                    <?php while($vendedor = mysqli_fetch_assoc($resultado)): ?>
+                        <option <?php echo $vendedorId === $vendedor['id'] ? 'selected' : ''; ?> value="<?php echo $vendedor['id']?>">
+                            <?php echo $vendedor['nombre']." ". $vendedor['apellido']; ?>
+                        </option>
+                    <?php endwhile; ?>
                 </select>
             </fieldset>
 
--- bienesraices_inicio/docu.sql
+++ bienesraices_inicio/docu.sql
@@ -1,88 +1,56 @@
--- CREATE DATABASE bienesRaices_crud; 
+CREATE TABLE vendedores (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    telefono VARCHAR(15) NOT NULL
+);
+--------
+CREATE TABLE propiedades (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    descripcion TEXT NOT NULL,
+    habitaciones INT NOT NULL,
+    banio INT NOT NULL,
+    estacionamiento INT NOT NULL,
+    vendedorId INT NOT NULL,
+    FOREIGN KEY (vendedorId) REFERENCES vendedores(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+----------------
+-- En caso de que ya esten creadas las tablas, para poder relacionarlas realizar 
+ALTER TABLE propiedades
+ADD CONSTRAINT fk_vendedor
+FOREIGN KEY (vendedorId) REFERENCES vendedores(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+----------------------------------------------------------------
 
--- CREATE TABLE propiedades( 
---     id INT(11) NOT NULL AUTO_INCREMENT, 
---     titulo VARCHAR(45) NOT NULL, 
---     precio DECIMAL(10,2) NOT NULL, 
---     imagen VARCHAR(200) NOT NULL, 
---     descripcion LONGTEXT NOT NULL, 
---     habitaciones INT(1) NOT NULL, 
---     baño INT(1) NOT NULL, 
---     estacionamiento INT(1) NOT NULL, 
---     creado DATE NOT NULL, 
---     PRIMARY KEY (id) 
--- );
+CREATE TABLE vendedores (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(60) NOT NULL,
+    apellido VARCHAR(60) NOT NULL,
+    telefono VARCHAR(11) NOT NULL,
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE vendedores( 
---     id INT(11) NOT NULL AUTO_INCREMENT, 
---     nombre VARCHAR(45) NOT NULL, 
---     apellido VARCHAR(45) NOT NULL, 
---     telefono VARCHAR(10) NOT NULL, 
---     PRIMARY KEY (id) 
--- );
+CREATE TABLE propiedades(
+    id INT(11) AUTO_INCREMENT,
+    titulo VARCHAR(255) NOT NULL,
+    precio DECIMAL(10,2) NOT NULL,
+    descripcion TEXT NOT NULL,
+    habitaciones INT NOT NULL,
+    banio INT NOT NULL,
+    estacionamiento INT NOT NULL,
+    vendedorId INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    KEY vendedorId (vendedorId),
+    CONSTRAINT vendedor_FK
+    FOREIGN KEY (vendedorId) 
+    REFERENCES vendedores(id)
+);
 
---------------------------------------------------------------------------
--- MySQL Workbench Forward Engineering
+INSERT INTO `vendedores` (`nombre`, `apellido`, `telefono`) VALUES
+('Natt', 'Techeira', '123456789'),
+('Braian', 'Zamudio', '987654321');
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema bienesraices_crud
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema bienesraices_crud
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `bienesraices_crud` DEFAULT CHARACTER SET utf8 ;
-USE `bienesraices_crud` ;
-
--- -----------------------------------------------------
--- Table `bienesraices_crud`.`vendedores`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bienesraices_crud`.`vendedores` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  `apellido` VARCHAR(45) NULL,
-  `telefono` VARCHAR(45) NULL,
-  `vendedorescol` VARCHAR(10) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bienesraices_crud`.`propiedades`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bienesraices_crud`.`propiedades` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `titulo` VARCHAR(45) NULL,
-  `precio` DECIMAL(10,2) NULL,
-  `imagen` VARCHAR(200) NULL DEFAULT CURRENT_TIMESTAMP,
-  `descripcion` LONGTEXT NULL,
-  `habitaciones` INT(1) NULL,
-  `baño` INT(1) NULL,
-  `estacionamiento` INT(1) NULL,
-  `creado` DATE NULL,
-  `vendedores_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_propiedades_vendedores_idx` (`vendedores_id` ASC) VISIBLE,
-  CONSTRAINT `fk_propiedades_vendedores`
-    FOREIGN KEY (`vendedores_id`)
-    REFERENCES `bienesraices_crud`.`vendedores` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `bienesraices_crud`.`table1`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `bienesraices_crud`.`table1` (
-)
-ENGINE = InnoDB;
-
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+ALTER TABLE `propiedades` ADD `creado` DATE NULL AFTER `estacionamiento`;
+ALTER TABLE `propiedades` ADD `imagen` VARCHAR(200) NOT NULL AFTER `precio`;
