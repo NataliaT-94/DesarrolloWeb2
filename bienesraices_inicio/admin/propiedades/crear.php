1a60c3b34e60ba8d6503093d56b426409a6693ff
NataliaT-94 <nattecheira@gmail.com>
2024-10-18 12:46:30 -0300

    bienesraices- subir archivos de imagenes a la base de datos
    crear carpeta de imagenes, colocarle un nombre unico,
    subir las imagenes a la base de datos

--- bienesraices_inicio/admin/propiedades/crear.php
+++ bienesraices_inicio/admin/propiedades/crear.php
@@ -27,64 +27,106 @@
     $vendedorId = '';
 
   //Ejecutar el codigo despues de que el usuario envia el formulario
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {//nos trae informacion detallada del servidor
       // Opcional: Mostrar los datos recibidos para depuración
         //   echo "<pre>";
-        //   var_dump($_POST);
+        //   var_dump($_POST);//nos trae la informacion ciando la enviamos por nuestro formulario
         //   echo "</pre>";
+
+        //   echo "<pre>";
+        //   var_dump($_FILES);// nos permite ver el contenido de los archivos
+        //   echo "</pre>";
+
+        
   
-      // Sanitizar y asignar variables
-      $titulo = $_POST['titulo'];
-      $precio = $_POST['precio'];
-      $descripcion = $_POST['descripcion'];
-      $habitaciones = $_POST['habitaciones'];
-      $banio = $_POST['banio'];
-      $estacionamiento = $_POST['estacionamiento'];
-      $vendedorId = $_POST['vendedor'];
-      $creado = date('y/m/d');
+        // Sanitizar: sirve para proteger tu codigo //asignar variables
+        $titulo = mysqli_real_escape_string($db, $_POST['titulo']);
+        $precio = mysqli_real_escape_string($db, $_POST['precio']);
+        $descripcion = mysqli_real_escape_string($db, $_POST['descripcion']);
+        $habitaciones = mysqli_real_escape_string($db, $_POST['habitaciones']);
+        $banio = mysqli_real_escape_string($db, $_POST['banio']);
+        $estacionamiento = mysqli_real_escape_string($db, $_POST['estacionamiento']);
+        $vendedorId = mysqli_real_escape_string($db, $_POST['vendedor']);
+        $creado = date('Y/m/d');
 
-      if(!$titulo){//si no hay titulo
-        $errores[] = "Debes añadir un titulo";
-      }
+        //asignar files hacia unavariable
+        $imagen = $_FILES['imagen'];
 
-      if(!$precio){
-        $errores[] = "Debes añadir un precio";
-      }
+        //var_dump($imagen['name']);
 
-      if(strlen($descripcion) < 50){
-        $errores[] = "La descripcion es oblidgatoria y debe tener al menos 50 caracteres";
-      }
+        //exit;
 
-      if(!$habitaciones){
-        $errores[] = "El numero de habitaciones es obligatorio";
-      }
+        if(!$titulo){//si no hay titulo
+            $errores[] = "Debes añadir un titulo";
+        }
 
-      if(!$banio){
-        $errores[] = "El numero de banios es obligatorio";
-      }
+        if(!$precio){
+            $errores[] = "Debes añadir un precio";
+        }
 
-      if(!$estacionamiento){
-        $errores[] = "El numero de estacionamientos es obligatorio";
-      }
+        if(strlen($descripcion) < 50){
+            $errores[] = "La descripcion es oblidgatoria y debe tener al menos 50 caracteres";
+        }
 
-      if(!$vendedorId){
-        $errores[] = "Elije un vendedor";
-      }
+        if(!$habitaciones){
+            $errores[] = "El numero de habitaciones es obligatorio";
+        }
 
-    //   echo "<pre>";
-    //   var_dump($errores);
-    //   echo "</pre>";
+        if(!$banio){
+            $errores[] = "El numero de banios es obligatorio";
+        }
 
-    //   exit;
+        if(!$estacionamiento){
+            $errores[] = "El numero de estacionamientos es obligatorio";
+        }
+
+        if(!$vendedorId){
+            $errores[] = "Elije un vendedor";
+        }
+
+        if(!$imagen['name'] || $imagen['error']){
+                $errores[] = "La imagen es obligatoria";
+        }
+
+        //Validar por tamaño (1mb maximo)
+        $medida = 1000 * 1000;
+
+        if($imagen['size'] > $medida){
+                $errores[] = "La imagen es muuy pesada";
+        }
+
+                // echo "<pre>";
+                // var_dump($errores);
+                // echo "</pre>";
+
+     
 
       //Revisar que el array de $errores ese vacio 
 
-      if(empty($errores)){//si el array de $errores esta vacio
+        if(empty($errores)){//si el array de $errores esta vacio
 
+            /** SUBIDA DE ARCHIVOS */
+
+            //Crear carpeta
+            $carpetaImagenes = '../../imagenes/';
+
+            if(!is_dir($carpetaImagenes)){//verifica que la carpeta este creada o no
+                mkdir($carpetaImagenes);//crea galeria
+            }
+
+            //Generar un nombre unico
+            $nombreImagen = md5(uniqid(rand(),true)) . ".jpg";
+
+            //Subir la imagen
+
+            move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen);
+
+            
+            exit;
             // insertar base de datos
-            $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, banio, estacionamiento, creado, vendedorId) 
-            VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$banio', '$estacionamiento', $creado, '$vendedorId')";
-        
+            $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion, habitaciones, banio, estacionamiento, creado, vendedorId) 
+            VALUES ('$titulo', '$precio', $nombreImagen, '$descripcion', '$habitaciones', '$banio', '$estacionamiento', $creado, '$vendedorId')";
+            
 
             //echo $query;
 
@@ -98,7 +140,7 @@
                 echo "Error: " . mysqli_error($db);
             }
         }
-  }
+    }
 
 
 
@@ -119,7 +161,7 @@
 
         <?php endforeach; ?>
 
-        <form class="formulario" method="POST" action="http://localhost/GitHub/DesarrolloWeb2/bienesraices_inicio/admin/propiedades/crear.php">
+        <form class="formulario" method="POST" action="http://localhost/GitHub/DesarrolloWeb2/bienesraices_inicio/admin/propiedades/crear.php" enctype="multipart/form-data"> <!-- enctype= sirve para subir archivos al formulario--------->
             <!-- GET: guarda la informacion en la URL, se comunica con el name: titulo// no es recomendable para mandar datos a un servidor, porque expone los datos en la URL -->
             <!-- POST: maneja internamente en el archivo, se comunica con el name: titulo // envia datos de forma segura -->
             <fieldset>
@@ -129,10 +171,10 @@
                 <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad" value="<?php echo $titulo; ?>">
                 
                 <label for="precio">Precio:</label>
-                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $preci0; ?>">
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $precio; ?>">
                 
                 <label for="imagen">Imagen:</label>
-                <input type="file" id="imagen" accept="image/jpeg, image/png">
+                <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
                 
                 <label for="descripcion">Descripcion:</label>
                 <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
