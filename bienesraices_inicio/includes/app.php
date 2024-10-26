6deb321addcb8317d757c22595449e2da72cc097
NataliaT-94 <nattecheira@gmail.com>
2024-10-26 15:43:35 -0300

    inicio bienesraices-POO

--- bienesraices_inicio/includes/app.php
+++ bienesraices_inicio/includes/app.php
@@ -1,5 +1,16 @@
 <?php
 
-define('TEMPLATES_URL', __DIR__. '/templates');//__DIR__ permite completar la ruta para poder acceder a templates, hay que entrar a la carpeta por eso lleva /
-define('FUNCIONES_URL', __DIR__ . '/includes/funciones.php');
-// esta en e l mismo nivel que app.php, por eso no lleva /
+require 'funciones.php';
+require 'config/database.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+//Conectar a la base de datos
+$db = conectarDB();
+
+use App\Propiedad;
+
+Propiedad::setDB($db);
+
+
+
+?>
