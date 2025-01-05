35a81b87633ab60096880b8b7c8f5d32d91e5655
NataliaT-94 <nattecheira@gmail.com>
2025-01-04 21:03:52 -0300

    nailsmvc-bd(sql)-login-scss-viws

--- nailsAppMVC/public/index.php
+++ nailsAppMVC/public/index.php
@@ -2,9 +2,26 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
+
+//Iniciar Sesion
+$router->get('/', [LoginController::class, 'login']);//login sale de la funcion login que esta en el archivo LoginController
+$router->post('/', [LoginController::class, 'login']);//post: es cuando lleno el formulario y lo envio
+$router->get('/logout', [LoginController::class, 'logouts']);//logout cuando cerramos sesion
+
+//Recuperar Password
+$router->get('/olvide', [LoginController::class, 'olvide']);
+$router->post('/olvide', [LoginController::class, 'olvide']);
+$router->get('/recuperar', [LoginController::class, 'recuperar']);
+$router->post('/recuperar', [LoginController::class, 'recuperar']);
+
+//Crear Cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crear']);
+$router->post('/crear-cuenta', [LoginController::class, 'crear']);
+
 
 //Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
