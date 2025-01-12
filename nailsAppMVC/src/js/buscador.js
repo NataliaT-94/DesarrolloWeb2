1cc996260d99d077ed540dce6ef983631355af0b
NataliaT-94 <nattecheira@gmail.com>
2025-01-12 04:19:36 -0300

    nailsAppMVC suma de precios y filtro por fecha

---
+++ nailsAppMVC/src/js/buscador.js
@@ -0,0 +1,19 @@
+const { logError } = require("gulp-dart-sass");
+
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+function iniciarApp(){
+    buscarPorFecha();
+}
+
+function buscarPorFecha(){
+    const fechaInput = document.querySelector('#fecha');
+    fechaInput.addEventListener('input', function(e){
+        const fechaSeleccionada = e.target.value;
+        
+        window.location = `?fecha=${fechaSeleccionada}`;
+        
+    });
+}
