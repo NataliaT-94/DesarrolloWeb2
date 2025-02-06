0210294742c64ea6df480a0006210138e132f73a
NataliaT-94 <nattecheira@gmail.com>
2025-02-06 18:19:25 -0300

    devwebCamp-login, crear cuenta, olvide, scss-BEM

---
+++ DevWebCamp/docu.sql
@@ -0,0 +1,23 @@
+-- 3 tablas
+-- -Usuarios(clientes y Admin)
+--     id int(11)
+--     nombre varchar(30)
+--     email varchar(30)
+--     password varchar(60)
+--     token varchar(15)
+--     confirmado tinyint(1)
+
+-- -Proyectos
+--     id int(11)
+--     proyecto varchar(60)
+--     url varchar(32)
+--     propietarioId int(11)(FK)
+
+-- -Tareas
+--     id int(11)
+--     nombre varchar(60)
+--     estado tinyint(1)
+--     proyectoId int(11)(FK)
+
+
+-----------------------------------------------
