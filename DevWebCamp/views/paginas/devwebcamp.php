b8d882e4bd6b8ce0579f943d1fae024c5b61dd18
NataliaT-94 <nattecheira@gmail.com>
2025-02-20 18:04:55 -0300

    DevWebCamp-paginas publicas- paginasController, evento, .paquetes

---
+++ DevWebCamp/views/paginas/devwebcamp.php
@@ -0,0 +1,19 @@
+<main class="devwebcamp">
+    <h2 class="devwebcamp__heading"><?php echo $titulo; ?></h2>
+    <p class="devwebcamp__descripcion">conoce la conferencia mas importante de Latinoamerica</p>
+
+    <div class="devwebcamp__grid">
+        <div class="devwebcamp__imagen">
+            <picture>
+                <source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+                <source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/sobre_devwebcamp.jpg" alt="Imagen Devwebcamp">
+            </picture>
+        </div>
+
+        <div class="devwebcamp__contenido">
+            <p class="devwebcamp__texto">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque odio id nihil voluptatum commodi, blanditiis sapiente itaque, eum ullam delectus vel quia quibusdam similique repellendus et laboriosam corrupti nam harum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum saepe laboriosam atque beatae iste est earum qui, impedit dolor culpa necessitatibus.</p>
+            <p class="devwebcamp__texto">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque odio id nihil voluptatum commodi, blanditiis sapiente itaque, eum ullam delectus vel quia quibusdam similique repellendus et laboriosam corrupti nam harum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum saepe laboriosam atque beatae iste est earum qui, impedit dolor culpa necessitatibus.</p>
+        </div>
+    </div>
+</main>
