6053728dbdb925a9c4383859fa331d4f6e2eef2c
NataliaT-94 <nattecheira@gmail.com>
2025-03-25 12:19:11 -0300

    DevWebCamp- workshop y conferencias- carrousel con Swiper- Mostrar los eventos en el Fronntend/backend

--- DevWebCamp/controllers/PaginasController.php
+++ DevWebCamp/controllers/PaginasController.php
@@ -2,7 +2,12 @@
 
 namespace Controllers;
 
+use Model\Dia;
+use Model\Hora;
 use MVC\Router;
+use Model\Evento;
+use Model\Ponente;
+use Model\Categoria;
 
 class PaginasController {
     public static function index(Router $router) {
@@ -30,10 +35,36 @@ class PaginasController {
     }
 
     public static function conferencias(Router $router) {
+        $eventos = Evento::ordenar('hora_id', 'ASC');
 
+        $eventos_formatedos = [];
+        foreach($eventos as $evento){
+            $evento->categoria = Categoria::find($evento->categoria_id);//con esto podemos imprimir el nombre de la categoria en la tabla, en ves de mostrar el numero del id
+            $evento->dia = Dia::find($evento->dia_id);
+            $evento->hora = Hora::find($evento->hora_id);
+            $evento->ponente = Ponente::find($evento->ponente_id);
+            
+            if($evento->dia_id === "1" && $evento->categoria_id === "1"){
+                $eventos_formatedos['conferencias_v'][] = $evento;                
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "1"){
+                $eventos_formatedos['conferencias_s'][] = $evento;
+            }
+
+            if($evento->dia_id === "1" && $evento->categoria_id === "2"){
+                $eventos_formatedos['workshops_v'][] = $evento;                
+            }
+
+            if($evento->dia_id === "2" && $evento->categoria_id === "2"){
+                $eventos_formatedos['workshops_s'][] = $evento;
+            }
+        }
+        // debuguear($eventos_formatedos);
 
         $router->render('paginas/conferencias', [
-            'titulo' => 'Conferencias y Workshops'
+            'titulo' => 'Conferencias y Workshops',
+            'eventos' => $eventos_formatedos
         ]);
     }
 }
