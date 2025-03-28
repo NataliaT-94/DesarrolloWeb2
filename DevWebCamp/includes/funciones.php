bdf6686ccb8854d72124337a8e870db769293a90
NataliaT-94 <nattecheira@gmail.com>
2025-03-28 11:44:19 -0300

    DevWebCamp- pagina principal  conferencia- imagenes ponentes - animacion con scroll- pagina 404

--- DevWebCamp/includes/funciones.php
+++ DevWebCamp/includes/funciones.php
@@ -13,7 +13,7 @@ function s($html) : string {
 }
 
 function pagina_actual($path) : bool{
-    return str_contains($_SERVER['PATH_INFO'], $path) ? true : false;
+    return str_contains($_SERVER['PATH_INFO'] ?? '/', $path) ? true : false;
 }
 
 
@@ -29,4 +29,11 @@ function is_admin() : bool{
         session_start();
     }
     return isset($_SESSION['admin']) && !empty($_SESSION['admin']);
+}
+
+function aos_animacion() : void{
+    $efectos = ['fade-up', 'fade-down,', 'fade-left', 'fade-right', 'flip-left', 'flip-right', 'zoom-in', 'zoom-in-up', 'zoom-in-down', 'zoom-out',];
+    $efecto = array_rand($efectos, 1);
+    // echo $efectos[$efecto];
+    echo 'data-aos="' . $efectos[$efecto] . '" ';
 }
