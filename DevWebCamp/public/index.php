5b4fb7915817f87fd371fda72ee28218ba164c59
NataliaT-94 <nattecheira@gmail.com>
2025-04-16 11:24:35 -0300

    DevWebCamp-seccion admin completado- regalos- grafica- Chartjs, registros

--- DevWebCamp/public/index.php
+++ DevWebCamp/public/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
 use Controllers\APIEventos;
+use Controllers\APIRegalos;
 use Controllers\APIPonentes;
 use Controllers\AuthController;
 use Controllers\EventosController;
@@ -51,6 +52,7 @@ $router->post('/admin/ponentes/eliminar', [PonentesController::class, 'eliminar'
 $router->get('/api/eventos-horario', [APIEventos::class, 'index']);
 $router->get('/api/ponentes', [APIPonentes::class, 'index']);
 $router->get('/api/ponente', [APIPonentes::class, 'ponente']);
+$router->get('/api/regalos', [APIRegalos::class, 'index']);
 
 
 $router->get('/admin/eventos', [EventosController::class, 'index']);
